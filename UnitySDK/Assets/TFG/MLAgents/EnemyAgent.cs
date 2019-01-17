1a5b3cbe74d6fc385c7df5afb72f7ad8c0b352f5
Vfreixes <vfreixes@gmail.com>
2019-01-17 14:22:18 +0100

    some tweaking

--- UnitySDK/Assets/TFG/MLAgents/EnemyAgent.cs
+++ UnitySDK/Assets/TFG/MLAgents/EnemyAgent.cs
@@ -83,42 +83,35 @@ public class EnemyAgent : Agent {
         Vector3 lookAtPlayer = player.transform.position - enemy.transform.position;
 
         float angle = Vector3.Angle(lookDir, lookAtPlayer);
-        /*
-        if(distToPlayer < 1)
-        {
-            AddReward(0.3f);
-            Done();
-        }*/
-
-        //own damage penalty
         
-        enemy.horizontal = vectorAction[0];
-        enemy.vertical = vectorAction[1];
-        /*
+        
+        //actions to enemy stats
+        enemy.horizontal = vectorAction[0]; //movement
+        enemy.vertical = vectorAction[1];   //movement
         enemy.rt = vectorAction[2] == 1.0f ? true : false; //heavy attack
         enemy.rb = vectorAction[3] == 1.0f ? true : false; //light attack
         enemy.lb = vectorAction[4] == 1.0f ? true : false; //block
-        */
+        enemy.c_h = vectorAction[5];  //rotation
 
-        if (vectorAction[0] == 1) enemy.horizontal = 1;
-        Debug.Log(vectorAction[0] +  "+" + enemy.horizontal);
-        if (vectorAction[0] == -1) enemy.horizontal = -1;
-        if (vectorAction[1] == 1) enemy.vertical = 1;
-        if (vectorAction[1] == -1) enemy.vertical = -1;
-        if (vectorAction[2] == 1) enemy.rt = true;
-        if (vectorAction[2] == 0) enemy.rt = false;
-        if (vectorAction[3] == 1) enemy.rb = true;
-        if (vectorAction[3] == 0) enemy.rb = false;
-        if (vectorAction[4] == 1) enemy.lb = true;
-        if (vectorAction[4] == 0) enemy.lb = false;
+        //if (vectorAction[0] == 1) enemy.horizontal = 1;
+        //if (vectorAction[0] == -1) enemy.horizontal = -1;
+        //if (vectorAction[1] == 1) enemy.vertical = 1;
+        //if (vectorAction[1] == -1) enemy.vertical = -1;
+        //if (vectorAction[2] == 1) enemy.rt = true;
+        //if (vectorAction[2] == 0) enemy.rt = false;
+        //if (vectorAction[3] == 1) enemy.rb = true;
+        //if (vectorAction[3] == 0) enemy.rb = false;
+        //if (vectorAction[4] == 1) enemy.lb = true;
+        //if (vectorAction[4] == 0) enemy.lb = false;
         //if (vectorAction[5] == 1) enemy.c_h = 1;
         //if (vectorAction[5] == -1) enemy.c_h = -1;
 
-        enemy.c_h = vectorAction[5];
-
-
         enemy.FixedTick(delta);
 
+
+        //Rewards and punishments
+        
+        //damage
         if (prevHP > enemy.curHP)
         {
             AddReward(-0.3f);
@@ -145,17 +138,24 @@ public class EnemyAgent : Agent {
         //objective reached
         if(player.curHP <= 0)
         {
-            AddReward(1.0f);
+            AddReward(2.0f);
             Done();
         }
 
+        if(enemy.curHP <= 0)
+        {
+            AddReward(-5.0f);
+            Done();
+        }
+        
         if(angle < 10)
         {
-            AddReward(0.3f);
+            AddReward(0.05f);
         }
         else
         {
-            AddReward(-0.3f);
+            AddReward(-0.08f);
         }
+        
     }
 }
