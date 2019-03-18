fb2af13faf7a0208371ed5ca73b6e8ecda8b7555
victor freixes <vfreixes@gmail.com>
2019-03-18 15:00:21 +0100

    FPS limit

--- UnitySDK/Assets/TFG/Scripts/InputHandler.cs
+++ UnitySDK/Assets/TFG/Scripts/InputHandler.cs
@@ -27,6 +27,9 @@ public class InputHandler : MonoBehaviour {
 
         camManager = CameraManager.singleton;
         camManager.Init(this.transform);
+
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
